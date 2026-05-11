a5f6565d11bcf6037bb319690da6a9317785ee06
SDemonteverde <38735789+Shyed@users.noreply.github.com>
2026-05-11 14:09:20 -0400

    Update DamageZone.cs

--- Assets/Scripts/DamageZone.cs
+++ Assets/Scripts/DamageZone.cs
@@ -1,15 +1,20 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using System.Collections; // allows use of basic collections like lists
+using System.Collections.Generic; // allows use of generic collections
+using UnityEngine; // import Unity game engine functions/classes
 
+// DamageZone Class: handles areas that damage the player
 public class DamageZone : MonoBehaviour
 {
+    // Called contiously while another object stays inside this trigger area
     void OnTriggerStay2D(Collider2D other)
     {
+        // Try to find RubyController component on the object touching the damage zone
         RubyController controller = other.GetComponent<RubyController>();
 
+        // Check if object actually has RubyController: prevents NPO touch triggers
         if (controller != null)
         {
+            // reduce player health by 1
             controller.ChangeHealth(-1);
         }
     }
